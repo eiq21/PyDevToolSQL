d14a9292e5aa8d16c71150b404c274e2a15a570c
eiq21 <inca.quispe.e@outlook.com>
2019-05-27 11:01:43 -0500

    Fix result sql string

--- FILES/OUT/query.sql
+++ FILES/OUT/query.sql
@@ -1,360 +1,348 @@
 USE BelcorpPeru_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
 USE BelcorpEcuador_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
 USE BelcorpColombia_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
 USE BelcorpDominicana_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
 USE BelcorpSalvador_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
 USE BelcorpCostaRica_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
 USE BelcorpBolivia_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
 USE BelcorpMexico_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
 USE BelcorpChile_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
 USE BelcorpGuatemala_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
 USE BelcorpPanama_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
 USE BelcorpPuertoRico_MC 
 GO 
-CREATE PROCEDURE [dbo].[GetCustomers_Pager]
-      @PageIndex INT = 1
-      ,@PageSize INT = 10
-      ,@RecordCount INT OUTPUT
-AS
-BEGIN
-      SET NOCOUNT ON;
-      SELECT ROW_NUMBER() OVER
-      (
-            ORDER BY [CustomerID] ASC
-      )AS RowNumber
-      ,[CustomerID]
-      ,[CompanyName]
-      ,[ContactName]
-      ,[City]
-      INTO #Results
-      FROM [Customers]
-
-      SELECT @RecordCount = COUNT(*)
-      FROM #Results
-
-      SELECT * FROM #Results
-      WHERE RowNumber BETWEEN(@PageIndex -1) * @PageSize + 1 AND(((@PageIndex -1) * @PageSize + 1) + @PageSize) - 1
-
-      DROP TABLE #Results
+
+ALTER PROCEDURE dbo.GetPedidosFacturadoDetalle_SB2 @PedidoID INT
+AS
+/*
+GetPedidosFacturadoDetalle_SB2 707193021
+*/
+BEGIN
+	SELECT --TOP 10
+		CA.CODIGO AS CampaniaID
+		,pd.CUV
+		,pd.cuvReemplazo as CUVReemplazo
+		,pc.Descripcion AS DescripcionProd
+		,pd.Cantidad
+		,pd.PedidoID
+		,pd.PrecioUnidad
+		,pd.PrecioTotal AS ImporteTotal
+	FROM ods.Pedido(NOLOCK) P
+	INNER JOIN ods.PedidoDetalle(NOLOCK) PD ON P.PedidoID = PD.PedidoID
+	INNER JOIN ods.ProductoComercial(NOLOCK) PC ON PD.CampaniaID = PC.CampaniaID
+		AND PD.CUV = PC.CUV
+	INNER JOIN ods.Campania(NOLOCK) CA ON P.CampaniaID = CA.CampaniaID
+	INNER JOIN ods.Consultora(NOLOCK) CO ON P.ConsultoraID = CO.ConsultoraID
+	WHERE PD.PedidoID = @PedidoID
+	ORDER BY CA.Codigo DESC
 END
 GO 
 
